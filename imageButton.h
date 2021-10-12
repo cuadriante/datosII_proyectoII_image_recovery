c2691e3c3d14d666a9b18222f3fb12ccc1e5d921
Gregory Alpízar <gregory.ac@estudiantec.cr>
2021-10-12 00:13:54 -0600

    Button created

--- imageButton.h
+++ imageButton.h
@@ -1,41 +1,68 @@
 #ifndef DATOSII_PROYECTOII_IMAGE_RECOVERY_IMAGEBUTTON_H
 #define DATOSII_PROYECTOII_IMAGE_RECOVERY_IMAGEBUTTON_H
 
-//using namespace std;
-//using namespace sf;
-//
-//class imageButton {
-//public:
-//    imageButton() {}
-//
-//    imageButton(string t, Vector2F size, int charSize, Color bgColor, Color textColor) {
-//        text.setString(t);
-//        text.setColor(textColor);
-//        text.setCharacterSize(charSize);
-//
-//        button.setSize(size);
-//        button.setFillColor(bgColor);
-//
-//    }
-//
-//    void setFont(Font &font) {
-//        text.setFont(font);
-//    }
-//
-//    void setBackColor(Color color) {
-//        button.setFillColor(color);
-//    }
-//
-//    void setTextColor(Color color) {
-//        text.setColor(color);
-//    }
-//
-//    void setPosition(Vector2f pos) {
-//        button.setPosition(pos);
-//
-//        float xPos = (pos.x + button.getGlobalBounds().width / 2) - (text.getGlobalBounds().width / 2);
-//        float yPos = (pos.y + button.getGlobalBounds().height / 2) - (text.getGlobalBounds().width / 2);;
-//    }
-//};
+using namespace std;
+using namespace sf;
+
+class imageButton {
+public:
+    imageButton() {}
+
+    imageButton(string t, Vector2f size, int charSize, Color bgColor, Color textColor) {
+        text.setString(t);
+        text.setColor(textColor);
+        text.setCharacterSize(charSize);
+
+        button.setSize(size);
+        button.setFillColor(bgColor);
+
+    }
+
+    void setFont(Font &font) {
+        text.setFont(font);
+    }
+
+    void setBackColor(Color color) {
+        button.setFillColor(color);
+    }
+
+    void setTextColor(Color color) {
+        text.setColor(color);
+    }
+
+    void setPosition(Vector2f pos) {
+        button.setPosition(pos);
+
+        float xPos = (pos.x + button.getGlobalBounds().width / 2) - (text.getGlobalBounds().width / 2);
+        float yPos = (pos.y + button.getGlobalBounds().height / 2) - (text.getGlobalBounds().width / 2);
+        text.setPosition({xPos, yPos});
+    }
+
+    void drawTo(RenderWindow &window) {
+        window.draw(button);
+        window.draw(text);
+    }
+
+    bool isMouseOver(RenderWindow &window) {
+        float mouseX = Mouse::getPosition(window).x;
+        float mouseY = Mouse::getPosition(window).y;
+
+        float btnPosX = button.getPosition().x;
+        float btnPosY = button.getPosition().y;
+
+        float btnxPosWidth = button.getPosition().x + button.getLocalBounds().width;
+        float btnyPosHeight = button.getPosition().y + button.getLocalBounds().height;
+
+        if(mouseX < btnxPosWidth && mouseX > btnPosX && mouseY < btnyPosHeight && mouseY > btnPosY) {
+            return true;
+        }
+
+        return false;
+    }
+
+private:
+    RectangleShape button;
+    Text text;
+};
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<SFML/Graphics.hpp>
 #include "ImageHandler.h"
+#include "imageButton.h"
 
 using namespace sf;
 
@@ -11,17 +12,33 @@ int main() {
 
     //sfml window
     RenderWindow window (VideoMode(1000, 800), "Image Recovery");
+
+    imageButton btn1("Click me", {200, 50}, 20, Color::Green, Color::Black);
+    btn1.setPosition({100, 300});
+
     while (window.isOpen())
     {
         Event event;
         while(window.pollEvent(event))
         {
-            if (event.type == Event::Closed)
-                window.close();
+            switch (event.type) {
+
+                case Event::Closed:
+                    window.close();
+
+                case Event::MouseMoved:
+                    if (btn1.isMouseOver(window)) {
+                        btn1.setBackColor(Color::White);
+                    } else {
+                        btn1.setBackColor(Color::Green);
+                    }
+
+            }
         }
         window.clear(Color(23,57,84,255));
+        btn1.drawTo(window);
         window.display();
-        imageHandler.storeImageAsPixels();
+//        imageHandler.storeImageAsPixels();
     }
     return 0;
 }
