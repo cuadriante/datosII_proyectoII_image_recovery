afe5e98e22284f860738854601290d7de907859b
Gregory Alpízar <gregory.ac@estudiantec.cr>
2021-11-04 22:34:41 -0600

    Update ColorInfo.h with Doxygen

--- ColorInfo.h
+++ ColorInfo.h
@@ -32,26 +32,71 @@ private:
 public:
     const int NORMALIZED_RANGE = 3;
 
+    /**
+     * @brief Constructor
+     * @param color Color con formato de SFML
+     */
     ColorInfo(Color color);
 
+    /**
+     * @brief Getter del código para el color rojo
+     * @return Un entero correspondiente al color rojo
+     */
     int getRed() const;
 
+    /**
+     * @brief Getter del código para el color verde
+     * @return Un entero correspondiente al color verde
+     */
     int getGreen() const;
 
+    /**
+     * @brief Getter del código para el color azul
+     * @return Un entero correspondiente al color azul
+     */
     int getBlue() const;
 
+    /**
+     * @brief Establece el color de un pixel
+     * @param newColor Color elegido por el usuario
+     */
     void setColor(const Color &newColor);
 
+    /**
+     * @brief Getter del color rojo normalizado
+     * @return Un entero correspondiente al color rojo normalizado
+     */
     int getNRed() const;
 
+    /**
+     * @brief Getter del color verde normalizado
+     * @return Un entero correspondiente al color verde normalizado
+     */
     int getNGreen() const;
 
+    /**
+     * @brief Getter del color azul normalizado
+     * @return Un entero correspondiente al color azul normalizado
+     */
     int getNBlue() const;
 
+    /**
+     * @brief Getter del color de un pixel en formato SFML
+     * @return Un valor del tipo Color
+     */
     const Color &getColor() const;
 
+    /**
+     * @brief Determina si dos objetos ColorInfo poseen colores similares
+     * @param colorInfo Color a comparar
+     * @return Una respuesta afirmativa o negativa de la comparación
+     */
     bool isSimilar(ColorInfo * colorInfo);
 
+    /**
+     * @brief Determina si un pixel se encuentra en escala de grises
+     * @return Una respuesta afirmativa o negativa de la comprobación
+     */
     bool isGrayScale();
 
 
